e4541f7eb425496ebccf0851ae4314ae5466b3d2
Rob-n-Dude <m.r.stickcw@yandex.ru>
2021-04-07 13:01:55 +0300

    fix: file-names

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,18 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  for (let i = 0; i < names.length; i++) {
+    let counter = 0;
+    for (let j = i + 1; j < names.length; j++) {
+      if (names[i] === names[j]) {
+        counter += 1;
+        const lineToPush = `${names[j]}(${counter})`;
+        names.splice(j, 1, lineToPush);
+      }
+    }
+  }
+  return names;
 }
 
 module.exports = renameFiles;
